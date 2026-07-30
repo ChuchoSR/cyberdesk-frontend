9cef635bfdc7759aaecdaee9f33ca8a0cfcefc53
Chucho <jesusjaviersierrarondon@gmail.com>
2026-07-30 14:33:14 -0400

    Solución del problema para uso de Tailwind

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss(),
+    
   ],
-  // Agregamos esto para forzar una sola instancia de React
+  // Regla de seguridad
   resolve: {
     dedupe: ['react', 'react-dom'],
   },
